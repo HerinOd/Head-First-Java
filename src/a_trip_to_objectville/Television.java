b202e4aac82c0d2e604cd720d6e4f771d37f4ac4
HerinOd <hieudo851@gmail.com>
2026-01-11 23:20:44 +0700

    Working on chapter 2 #2

--- src/a_trip_to_objectville/Television.java
+++ src/a_trip_to_objectville/Television.java
@@ -1,5 +1,8 @@
 package a_trip_to_objectville;
+// Page 34
 
+//Fill in what a television object
+//might need to know and do.
 public class Television {
     //instance variables
     int[] channels;
